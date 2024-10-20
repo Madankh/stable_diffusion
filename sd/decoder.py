d7f31c54ba1e34c0e7fe6de82c7fa215da4e2d19
Madankh <khadkasuman0000@gmail.com>
2024-10-20 19:22:30 +0545

    update

--- sd/decoder.py
+++ sd/decoder.py
@@ -57,13 +57,38 @@ class VAE_ResidualBlock(nn.Module):
 class VAE_Decoder(nn.Sequential):
     def __init__(self):
         super().__init__(
+
             nn.Conv2d(4,4,kernel_size=1, padding=0),
+
             nn.Conv2d(4, 512, kernel_size=3, padding=1),
+
             VAE_ResidualBlock(512 , 512),
-            VAE_AttentionBlock(512)
-            VAE_ResidualBlock(512, 512),
-            VAE_ResidualBlock(512, 512),
-            VAE_ResidualBlock(512, 512),
+
+            VAE_AttentionBlock(512),
+
             VAE_ResidualBlock(512, 512),
 
-        )
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+            
+            # (Batch_size , 512, Height/8, Width/8) -- > # (Batch_size , 512, Height/8, Width/8)
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_size , 512, Height/8, Width/8) ---> # (Batch_size , 512, Height/4, Width/4)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+            
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_size , 512, Height/4, Width/4) ---> (Batch_size , 512, Height/2, Width/2)
+            nn.Upsample(scale_factor=2),
+        )   
+
+
+        
