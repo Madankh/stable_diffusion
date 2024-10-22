43773ca912d04144ff293d5dec7a677b39292f37
Madankh <khadkasuman0000@gmail.com>
2024-10-22 19:33:14 +0545

    clip

--- sd/clip.py
+++ sd/clip.py
@@ -6,3 +6,20 @@ from attention import SelfAttention
 class CLIP(nn.Module):
     def __init__(self):
         self.embedding = CLIPEmbedding(49408, 768, 77)
+
+        self.layers = nn.Module([
+            CLIPLayer(12, 768) for i in range(12)
+        ])
+        self.layernorm = nn.LayerNorm(768)
+
+    def forward(self, tokens:torch.LongTensor)->torch.FloatTensor:
+        tokens = tokens.type(torch.long)
+
+        # (Batch_Size, seq_len) -> (Batch_Size, seq_len, Dim)
+        state = self.embedding(tokens)
+
+        for layer in self.layers:
+            state = layer(state)
+        output = self.layernorm(state)
+
+        return output
