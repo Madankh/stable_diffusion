26bfb9f0ed3c6c959831bd282297d8e4506fb4cb
Madankh <khadkasuman0000@gmail.com>
2024-10-28 18:27:27 +0545

    diffusion improvement

--- sd/diffusion.py
+++ sd/diffusion.py
@@ -17,6 +17,77 @@ class TimeEmbedding(nn.Module):
 
         return x
     
+class UNET_ResidualBlock(nn.Module):
+    def __init__(self, in_channels:int , out_channels:int , n_time=1280):
+        super().__init__()
+        self.groupnorm_feature = nn.GroupNorm(32, in_channels)
+        self.conv_feature = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.linear_time = nn.Linear(n_time, out_channels)
+
+        self.groupnorm_merged = nn.GroupNorm(32, out_channels)
+        self.conv_merged = nn.Conv2d(out_channels, out_channels, kernel_size=1, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=0)
+        
+    def forward(self, feature, time):
+        # Feature : (Batch_size, In_Channels, Height, Width)
+        # Time (1, 1280)
+        residue = feature
+        feature = self.groupnorm_feature(feature)
+        feature = F.silu(feature)
+        feature = self.conv_feature(feature)
+        time = F.silu(time)
+        time = self.linear_time(time)
+        merged = self.groupnorm_merged(merged)
+        merged = F.silu(merged)
+        merged = self.conv_merged(merged)
+        return merged + self.residual_layer(residue)
+
+
+class UNET_AttentionBlock(nn.Module):
+    def __init__(self, n_head:int, n_embd:int, d_context=768):
+        super().__init__()
+        channels = n_head * n_embd
+
+        self.groupnorm = nn.GroupNorm(32, channels, eps=1e-6)
+        self.conv_input = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+        self.layernorm_1 = nn.LayerNorm(channels)
+        self.attention_1 = SelfAttention(n_head, channels, in_proj_bias=False)
+        self.layernorm_2 = nn.LayerNorm(channels)
+        self.attention_2 = CrossAttention(n_head, channels, d_context, in_proj_bias=False)
+        self.layernorm_3 = nn.LayerNorm(channels)
+        self.linear_geglu_1 = nn.Linear(channels, 4*channels * 2)
+        self.linear_geglu_2 = nn.Linear(channels * 4, channels)
+
+        self.conv_output = nn.Conv2d(channels, channels, kernel_size=3, padding=0)
+
+
+    def forward(self, x, context):
+        # X : (Batch_size, Features , Height, Width)
+        # Context : (Batch_size, seq_len, Dim)
+
+        residue_long = x
+        
+        x = self.groupnorm(x)
+
+        x = self.conv_input(x)
+
+        n, c, h, w = x.shape
+        # (Batch_size, Features, Height, Width) -> (Batch_size, Features, Height * Width)
+        x = x.view((n, c, h*w))
+        
+        x = x.transpose(-1, -2)
+
+        # Normalization + Self Attention with skin connection
+        
+
+
+
+
 class SwitchSequential(nn.Sequential):
     def forward(self, x:torch.Tensor, context:torch.Tensor, time:torch.Tensor):
         for layer in self:
