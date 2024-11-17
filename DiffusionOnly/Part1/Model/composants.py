8597038f65cbd8684f8b905613859b203f8eb37e
Madankh <khadkasuman0000@gmail.com>
2024-11-17 17:32:43 +0545

    composants

--- DiffusionOnly/Part1/Model/composants.py
+++ DiffusionOnly/Part1/Model/composants.py
@@ -3,4 +3,40 @@ import torch.nn as nn
 import math
 from einops.layers.torch import Rearrange
 from einops import rearrange
-from ai_utils import default
+from utils import default
+
+class ConvNextBlock(nn.Module):
+    def __init__(
+            self,
+            in_channels, 
+            out_channels, 
+            mult=2, 
+            time_embedding_dim=None, 
+            norm=True, 
+            group=8):
+        super().__init__()
+        self.mlp = (
+            nn.Sequential(
+                nn.GELU(),
+                nn.Linear(time_embedding_dim, in_channels)
+            )
+            if time_embedding_dim
+            else None
+        )
+
+        self.in_conv = nn.Conv2d(in_channels, in_channels, 7, padding=3,groups=in_channels)
+        self.block = nn.Seqential(
+            nn.GroupNorm(1,in_channels) if norm else nn.Identity(),
+            nn.Conv2d(in_channels, out_channels * mult, 3, padding=1),
+            nn.GELU(),
+            nn.GroupNorm(1, out_channels*mult),
+            nn.Conv2d(out_channels * mult , out_channels, 3, padding=1)
+        )
+
+        self.residual_conv = (
+            nn.Conv2d(in_channels, out_channels, 1)
+            if in_channels != out_channels
+            else nn.Identity()
+        )
+
+        
