d60bdd1401a7af11a9e8b45a4f9f45a6b21977c8
Madankh <khadkasuman0000@gmail.com>
2024-11-21 16:14:34 +0545

    modules-complete

--- DiffusionOnly/modules.py
+++ DiffusionOnly/modules.py
@@ -143,7 +143,127 @@ class Attend(nn.Module):
         
 
 class Attention(nn.Module):
-    def __init__(self, dim, heads = 4, dim_head=32):
+    def __init__(
+        self,
+        dim,
+        heads = 4,
+        dim_head = 32
+    ):
         super().__init__()
         self.heads = heads
         hidden_dim = dim_head * heads
+
+        self.norm = RMSNorm(dim)
+        self.attend = Attend()
+
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias = False)
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x = self.norm(x)
+        qkv = self.to_qkv(x).chunk(3, dim = 1)
+        q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> b h (x y) c', h = self.heads), qkv)
+        out = self.attend(q, k, v)
+        out = rearrange(out, 'b h (x y) d -> b (h d) x y', x = h, y = w)
+        return self.to_out(out)
+    
+class Unet(nn.Module):
+    def __init__(
+            self,
+            dim,
+            init_dim=None,
+            out_dim=None,
+            dim_mults=(1,2,4,8),
+            channels=3,
+            resnet_block_groups=8
+            ):
+        super().__init__()
+
+        # determine dimensions
+        self.channels = channels # input channels 
+        input_channels = channels
+
+        init_dim = default(init_dim, dim)  #Initial dimension of the featuere map extracted from the input image
+        self.init_conv = nn.Conv2d(input_channels, init_dim, 7, padding=3)
+
+        dims =  [init_dim,  *map(lambda m:dim * m, dim_mults)] # Builds multiples of the initial dimensions, used for the encoder and the decoder
+        in_out = list(zip(dims[:-1],dim[1:])) # Builds a map of input-output dimensions for the encoder and the decoder
+        
+        block_klass = partial(ResnetBlock, groups=resnet_block_groups) # Initializes the number of groups for Group Normalization
+
+        # time embeddings
+        time_dim = dim*4 #  the size of the time embedding
+        sinu_pos_emb = SinusoidalPosEmb(dim) # Positional encodings just like in normal transfomer
+        self.time_mlp = nn.Sequential(
+            sinu_pos_emb,
+            nn.Linear(dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim)
+        )
+
+        # layerss
+        self.downs  = nn.modules([])
+        self.ups = nn.modules([])
+        num_resolutions = len(in_out) # number of layers (up and downs) in the unet
+
+        for ind, ((dim_in, dim_out)) in enumerate(in_out):
+            is_last  = ind >= (num_resolutions -  1)
+
+            self.downs.append(nn.ModuleList([
+                block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                Attention(dim_in),
+                Downsample(dim_in, dim_out) if not is_last else nn.Conv2d(dim_in, dim_out,3, padding=1)
+            ]))
+
+        mid_dim = dims[:-1]  # The dimension of the middle layer of the UNet = 512
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_attn = Attention(mid_dim)
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+
+        for ind, ((dim_in, dim_out)) in enumerate(reversed(in_out)):
+            is_last = ind == (len(in_out) - 1)
+
+            self.ups.append(nn.ModuleList([
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim = time_dim),
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim = time_dim),
+                Attention(dim_out),
+                Upsample(dim_out, dim_in) if not is_last else  nn.Conv2d(dim_out, dim_in, 3, padding = 1)
+            ]))
+        default_out_dim = channels
+        self.out_dim = default(out_dim, default_out_dim) # channels of the output image
+
+        self.final_res_block = block_klass(dim*2, dim, time_emb_dim=time_dim)
+        self.final_conv =  nn.Conv2d(dim,  self.out_dim, 1)
+
+    def forward(self, x,  time):
+        x = self.init_conv(x)
+        r = x.clone()
+        t = self.time_mlp(time)
+
+        h = []
+
+        for block1, block2, attn, downsample in self.downs:
+            x = block1(x, t)
+            h.append(x)
+            x = block2(x, t)
+            x = attn(x)
+            h.append(x)
+            x = downsample(x)
+        
+        x = self.mid_block1(x, t)
+        x  = self.mid_attn(x) + x
+        x = self.mid_block2(x,  t)
+
+        for block1, block2, attn, downsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block1(x, t)
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block2(x, t)
+            x = attn(x) + x
+            x = Upsample(x)
+
+        x = torch.cat((x,r), dim=1)
+        x = self.final_res_block(x, t)
+        return self.final_conv(x)
