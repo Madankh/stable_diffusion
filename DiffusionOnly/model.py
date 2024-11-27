55bff12088da47c195db3cdd1a7ef72034fc7e4a
Madankh <khadkasuman0000@gmail.com>
2024-11-27 17:41:00 +0545

    model

--- DiffusionOnly/model.py
+++ DiffusionOnly/model.py
@@ -27,4 +27,46 @@ class DiffusionModel(nn.Module):
     def alpha_bar(self,t):
         return math.prod([self.alpha(j) for j in range(t)])
     
+    def get_loss(self, batch, batch_idx):
+        """
+        Corresponds to Algorithm 1 from 
+        """
+        # Get a random time step for each image in the batch
+        ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
+        noise_imgs = []
+        # Generate noise one for each image in the batch
+        epsilons = torch.randn(batch.shape, device=self.device)
+
+        for i in range(len(ts)):
+            a_hat = self.alpha_bar(ts[i])
+            noise_imgs.append(
+                (math.sqrt(a_hat) * batch[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
+            )
+        noise_imgs = torch.stack(noise_imgs, dim=0)
+        # Run the noisy images through the U-Net to get predicted Noise
+        e_hat = self.forward(noise_imgs, ts)
+        # calculate the loss,  that is the MSE between the predicted noise and the actual noise
+        loss = nn.functional.mse_loss(
+            e_hat.reshape(-1, self.in_size), epsilons.reshape(-1, self.in_size)
+        )
+
+        return loss
+    
+    def denoise_sample(self, x, t):
+        """Corresponds to the inner loop of the algo 2 from 
+        (Ho et al., 2020)."""
+        with torch.no_grad():
+            if t>1:
+                z = torch.randn(x.shape)
+            else:
+                z = 0
+            # Get the predicted noise from the U-Net
+            e_hat = self.forward(x,  t.view(1).repeat(x.shape[0]))
+            # Preform the denosing step to take the image from t to t-1
+            pre_scale = 1/math.sqrt(self.alpha(t))
+            e_scale = (1 - self.alpha(t))/math.sqrt(1 - self.alpha_bar[t]) 
+            post_sigma = math.sqrt(self.beta(t)) * z
+            x = pre_scale * (x - e_scale * e_hat) + post_sigma
+            return x
+    
     
